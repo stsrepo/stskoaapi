6d70c3967cec1518fdde8a5a76f0496c0d712913
stsrepo <51185697+stsrepo@users.noreply.github.com>
2019-12-06 17:08:42 +0530

    new changes for REACT Application

--- server.js
+++ server.js
@@ -2,11 +2,39 @@ const Koa  = require('koa')
 const logger = require('koa-morgan')
 const Router = require('koa-router')
 const bodyParser = require('koa-body')()
+const cors = require('koa2-cors')
 
 const models = require('./models/')
 
 const server = new Koa() 
 const router = new Router()
+
+const options = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
+
+const koaOptions = {
+    origin: true,
+    credentials: true
+};
+
+router.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Credentials', 'true');
+    ctx.set('Access-Control-Allow-Origin', '*');
+    await next();
+  });
+
+router.options('*', async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set(
+      'Access-Control-Allow-Headers',
+      ctx.get('Access-Control-Request-Headers')
+    );
+    ctx.status = 204;
+    await next();
+  });
 
 router.get('/shipmentdetails', async ctx => {
     const shipment = await models.Shipmentdetails.findAll()
@@ -24,25 +52,25 @@ router.get('/shipmentdetails/:id', async ctx => {
 
 router.post('/shipmentdetails', bodyParser, async ctx => {
     const shipment = await models.Shipmentdetails.create(ctx.request.body)
-    const successmsg = {
+    const response = {
         "status":true,
         "message":"Insertion Successfull"
     }
     ctx.body = {
-        //successmsg
-        shipment
+        response
+        //shipment
     }
 })
 
 router.put('/shipmentdetails/:id', bodyParser, async ctx => {
     let shipment = await models.Shipmentdetails.findOne({where: { id: ctx.params.id }})
     shipment = await shipment.update(ctx.request.body)
-    const successmsg = {
+    const response = {
         "status":true,
         "message":"Updation Successfull"
     }
     ctx.body = {
-        successmsg
+        response
     }
 })
 
@@ -61,9 +89,26 @@ router.del('/shipmentdetails/:id', bodyParser, async ctx => {
 
 
 
+  
+
+  
 server
 .use(logger('tiny')) 
 .use(router.routes())
+.use(router.allowedMethods())
+.use(cors(({
+    origin: '*',
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  })))
+.use(cors(options))
+
+.use(function *(){
+    this.set('Access-Control-Allow-Origin', '*');
+})
 .use(ctx => {
     ctx.body = 'Hello Welcome to POC API Demo'
 })
